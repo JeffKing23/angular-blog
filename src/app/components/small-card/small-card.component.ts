9043516598fd219776fc04eeb5096944de03097e
Jefferson <gel_gui@hotmail.com>
2023-05-28 01:59:03 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+
+  @Input()
+  photoCover:string = "https://richestsoft.com/blog/wp-content/uploads/2022/12/What-is-Angular.webp"
+  
+  @Input()
+  cardTitle:string = "Quer conhecer mais sobre o Angular"
+
+  constructor() {}
+
+  ngOnInit(): void {    
+  }
 
 }
